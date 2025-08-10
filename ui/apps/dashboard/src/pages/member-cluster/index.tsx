a1dedfaf357741519711963cd85e1dbe8cf920ed
warjiang <1096409085@qq.com>
2025-08-11 00:20:40 +0800

    chore: opt for member-cluster

--- ui/apps/dashboard/src/pages/member-cluster/index.tsx
+++ ui/apps/dashboard/src/pages/member-cluster/index.tsx
@@ -1,18 +1,16 @@
-import { useCluster } from "@/hooks/useCluster.ts";
-import { useParams, Outlet, useNavigate } from "react-router-dom";
-import MemberClusterSidebar from "./components/sidebar";
-import Header from "@/layout/header";
-import { Layout as AntdLayout } from "antd";
-import { cn } from "@/utils/cn.ts";
-import { getSidebarWidth } from "@/utils/i18n";
-import { useWindowSize } from "@uidotdev/usehooks";
+import { useParams, Outlet } from 'react-router-dom';
+import MemberClusterSidebar from './components/sidebar';
+import Header from '@/layout/header';
+import { Layout as AntdLayout } from 'antd';
+import { cn } from '@/utils/cn.ts';
+import { getSidebarWidth } from '@/utils/i18n';
+import { useWindowSize } from '@uidotdev/usehooks';
 
 const { Sider: AntdSider, Content: AntdContent } = AntdLayout;
 
 export default function MemberCluster() {
   const { width } = useWindowSize();
   const isSmallScreen = width !== null && width <= 768;
-
 
   const params = useParams<{
     memberCluster: string;
@@ -29,7 +27,7 @@ export default function MemberCluster() {
 
       {/* content */}
       <AntdLayout
-        className={cn("h-[calc(100vh-48px)]", "overflow-hidden", "flex")}
+        className={cn('h-[calc(100vh-48px)]', 'overflow-hidden', 'flex')}
       >
         <AntdSider
           width={getSidebarWidth()}
