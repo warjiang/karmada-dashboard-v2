c1122b1959775dd9f3dc34197274993a850f20ea
warjiang <1096409085@qq.com>
2025-09-03 11:48:23 +0800

    fix(karmada-dashboard): add auth check in middleware
    Signed-off-by: warjiang <1096409085@qq.com>

--- cmd/api/app/router/setup.go
+++ cmd/api/app/router/setup.go
@@ -36,6 +36,7 @@ func init() {
 	router = gin.Default()
 	_ = router.SetTrustedProxies(nil)
 	v1 = router.Group("/api/v1")
+	v1.Use(AuthMiddleware())
 	member = v1.Group("/member/:clustername")
 	member.Use(EnsureMemberClusterMiddleware())
 
