f4065d07bd979984b83d23f4ceeca530f388a79c
warjiang <1096409085@qq.com>
2025-11-26 13:48:47 +0800

    feat(member-cluster-ui): overview wip2
    Signed-off-by: warjiang <1096409085@qq.com>

--- ui/apps/dashboard/src/pages/member-cluster/workload/deployments.tsx
+++ ui/apps/dashboard/src/pages/member-cluster/workload/deployments.tsx
@@ -1,12 +1,16 @@
-import { Table, Tag, Button, Space, Input, Select} from 'antd';
-import { EyeOutlined, EditOutlined } from '@ant-design/icons';
+import { Button, Input, Select, Space, Table, TableColumnProps } from 'antd';
+import { EditOutlined, EyeOutlined } from '@ant-design/icons';
 import { useMemberClusterContext } from '../hooks';
-import {useQuery} from "@tanstack/react-query";
-import {WorkloadKind} from "@/services";
-import {useState} from "react";
-import {GetMemberClusterWorkloads} from "@/services/member-cluster/workload.ts";
-import useNamespace from "../../../hooks/use-namespace.ts";
-import i18nInstance from "@/utils/i18n.tsx";
+import { useQuery } from '@tanstack/react-query';
+import { WorkloadKind } from '@/services';
+import { useState } from 'react';
+import {
+  GetMemberClusterWorkloads,
+  Workload,
+} from '@/services/member-cluster/workload.ts';
+import useNamespace from '../../../hooks/use-namespace.ts';
+import i18nInstance from '@/utils/i18n.tsx';
+import dayjs from 'dayjs';
 
 export default function MemberClusterDeployments() {
   const { memberClusterName } = useMemberClusterContext();
@@ -20,67 +24,73 @@ export default function MemberClusterDeployments() {
     searchText: '',
   });
   const { nsOptions, isNsDataLoading } = useNamespace({});
-    const { data, isLoading, refetch } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [memberClusterName, 'GetWorkloads', JSON.stringify(filter)],
     queryFn: async () => {
-      const clusters = await GetMemberClusterWorkloads({
-          memberClusterName: memberClusterName,
+      const workloads = await GetMemberClusterWorkloads({
+        memberClusterName: memberClusterName,
         kind: filter.kind,
         namespace: filter.selectedWorkSpace,
         keyword: filter.searchText,
       });
-      return clusters.data || {};
+      console.log('workloads.data', workloads);
+      return workloads;
     },
   });
 
-  const getStatusTag = (status: string) => {
-    const color = status === 'Running' ? 'success' : status === 'Pending' ? 'processing' : 'error';
-    return <Tag color={color}>{status}</Tag>;
-  };
-
-  const columns = [
+  const columns: TableColumnProps<Workload>[] = [
     {
       title: 'Name',
       dataIndex: 'name',
       key: 'name',
-      render: (name: string) => <strong>{name}</strong>
+      render: (_name: string, record: Workload) => (
+        <>{record.objectMeta.name}</>
+      ),
     },
     {
       title: 'Namespace',
       dataIndex: 'namespace',
-      key: 'namespace'
+      key: 'namespace',
+      render: (_name: string, record: Workload) => (
+        <>{record.objectMeta.namespace}</>
+      ),
     },
     {
       title: 'Ready',
       dataIndex: 'replicas',
-      key: 'replicas'
-    },
-    {
-      title: 'Status',
-      dataIndex: 'status',
-      key: 'status',
-      render: (status: string) => getStatusTag(status)
+      key: 'replicas',
+      render: (_status: string, record: Workload) => (
+        <>
+          {record.pods?.running}/{record.pods?.desired}
+        </>
+      ),
     },
     {
       title: 'Images',
       dataIndex: 'images',
       key: 'images',
-      render: (images: string) => <code className="text-xs">{images}</code>
+      render: (_, record: Workload) => (
+        <code className="text-xs">{record.containerImages?.[0]}</code>
+      ),
     },
     {
       title: 'Age',
       dataIndex: 'age',
-      key: 'age'
+      key: 'age',
+      render: (_, r) => {
+        const create = dayjs(r.objectMeta.creationTimestamp);
+        return create.fromNow();
+      },
     },
     {
       title: 'Actions',
       key: 'actions',
       render: () => (
         <Space>
-          <Button icon={<EyeOutlined />}  title="View details">
+          <Button icon={<EyeOutlined />} title="View details">
             View
           </Button>
-          <Button icon={<EditOutlined />}  title="Edit deployment">
+          <Button icon={<EditOutlined />} title="Edit deployment">
             Edit
           </Button>
           {/* 
@@ -88,10 +98,10 @@ export default function MemberClusterDeployments() {
             Delete
           </Button> */}
         </Space>
-      )
-    }
+      ),
+    },
   ];
-
+  console.log('x', data);
   return (
     <div className="h-full w-full flex flex-col p-4">
       <div className={'flex flex-row space-x-4 mb-4'}>
@@ -99,51 +109,53 @@ export default function MemberClusterDeployments() {
           {i18nInstance.t('280c56077360c204e536eb770495bc5f', '命名空间')}：
         </h3>
         <Select
-            options={nsOptions}
-            className={'min-w-[200px]'}
-            value={filter.selectedWorkSpace}
-            loading={isNsDataLoading}
-            showSearch
-            allowClear
-            onChange={(v) => {
-              setFilter({
-                ...filter,
-                selectedWorkSpace: v,
-              });
-            }}
+          options={nsOptions}
+          className={'min-w-[200px]'}
+          value={filter.selectedWorkSpace}
+          loading={isNsDataLoading}
+          showSearch
+          allowClear
+          onChange={(v) => {
+            setFilter({
+              ...filter,
+              selectedWorkSpace: v,
+            });
+          }}
         />
         <Input.Search
-            placeholder={i18nInstance.t(
-                'cfaff3e369b9bd51504feb59bf0972a0',
-                '按命名空间搜索',
-            )}
-            className={'w-[300px]'}
-            onPressEnter={(e) => {
-              const input = e.currentTarget.value;
-              setFilter({
-                ...filter,
-                searchText: input,
-              });
-            }}
+          placeholder={i18nInstance.t(
+            'cfaff3e369b9bd51504feb59bf0972a0',
+            '按命名空间搜索',
+          )}
+          className={'w-[300px]'}
+          onPressEnter={(e) => {
+            const input = e.currentTarget.value;
+            setFilter({
+              ...filter,
+              searchText: input,
+            });
+          }}
         />
       </div>
 
       <div className="flex-1 flex flex-col">
         <Table
+          rowKey={(record) =>
+            `${record.objectMeta.namespace}-${record.objectMeta.name}`
+          }
           columns={columns}
-          dataSource={data?.items || []}
-          rowKey="name"
+          dataSource={data?.deployments || []}
           pagination={{
             pageSize: 10,
             showSizeChanger: true,
             showQuickJumper: true,
-            showTotal: (total, range) => `${range[0]}-${range[1]} of ${total} deployments`
+            showTotal: (total, range) =>
+              `${range[0]}-${range[1]} of ${total} deployments`,
           }}
           scroll={{ y: 'calc(100vh - 400px)' }}
           loading={isLoading}
         />
       </div>
-
     </div>
   );
 }
