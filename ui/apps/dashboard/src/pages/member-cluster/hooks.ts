48fa665c66e25e093254dcddb355db33fba2cfc3
warjiang <1096409085@qq.com>
2026-02-02 00:23:29 +0800

    refactor: remove unused member cluster hooks file

--- ui/apps/dashboard/src/pages/member-cluster/hooks.ts
+++
@@ -1,9 +0,0 @@
-import { useOutletContext } from 'react-router-dom';
-
-type MemberClusterContext = {
-  memberClusterName: string;
-};
-
-export function useMemberClusterContext(): MemberClusterContext {
-  return useOutletContext<MemberClusterContext>();
-}
